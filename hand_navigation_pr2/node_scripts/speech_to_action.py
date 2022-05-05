33675d6e25ff8aa72702da2438ecab6848fa9925
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-05-05 14:55:03 +0900

    Update give-hand-init node

--- hand_navigation_pr2/node_scripts/speech_to_action.py
+++ hand_navigation_pr2/node_scripts/speech_to_action.py
@@ -2,30 +2,30 @@
 
 from speech_recognition_msgs.msg import SpeechRecognitionCandidates
 import rospy
-import actionlib
-import tf
-from nav_msgs.msg import Odometry
-import math
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryActionGoal
+# import actionlib
+# import tf
+# from nav_msgs.msg import Odometry
+# import math
+# from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+# from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryActionGoal
 
 
 class SpeechToAction():
 
     def __init__(self):
-        self.move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-        self.move_base_client.wait_for_server()
-        self.move_base_trajectry_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-        self.move_base_trajectry_client.wait_for_server()
+        # self.move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        # self.move_base_client.wait_for_server()
+        # self.move_base_trajectry_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        # self.move_base_trajectry_client.wait_for_server()
 
-        self.sub = rospy.Subscriber('~input', SpeechRecognitionCandidates, self.callback)
+        self.sub = rospy.Subscriber('~input', SpeechRecognitionCandidates, self.cb)
 
     def cb(self, msg):
         speech = msg.transcript[0]
-
-        if speech == "ストップ":
-            self.move_base_client.cancel_all_goals()
-            self.move_base_trajectry_client.cancel_all_goals()
+        print(speech)
+        # if speech == "ストップ":
+        #     self.move_base_client.cancel_all_goals()
+        #     self.move_base_trajectry_client.cancel_all_goals()
 
 if __name__ == '__main__':
     rospy.init_node('speech_to_action')
