2ba9dfc0a24ec8516a472c93fd8b5f92d4730b23
nakane11 <nakane-ao@g.ecc.u-tokyo.ac.jp>
2022-05-05 19:49:27 +0900

    Add velocity_filter

---
+++ hand_navigation_pr2/node_scripts/velocity_filter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from hand_navigation_pr2.msg import VelocityFilterAction, VelocityFilterResult
+from geometry_msgs.msg import Twist
+from jsk_topic_tools import ConnectionBasedTransport
+import rospy
+
+class VelocityFilter(ConnectionBasedTransport):
+
+    def __init__(self):
+        super(VelocityFilter, self).__init__()
+        self.server = actionlib.SimpleActionServer('velocity_filter', VelocityFilterAction, self.action_cb, False)
+        self.pub = rospy.Publisher('~output', Twist, queue_size=1)
+
+    def subscribe(self):
+        self.sub = rospy.Subscriber('~input', Twist, self.subscribe_cb)
+
+    def unsubscribe(self):
+        self.sub.unregister()
+
+    def subscribe_cb(self, msg):
+        filtered_msg = Twist()
+        filtered_msg.linear.x = msg.linear.x * self.speed
+        filtered_msg.linear.y = msg.linear.y * self.speed
+        filtered_msg.linear.z = msg.linear.z * self.speed        
+        filtered_msg.angular.x = msg.angular.x * self.speed
+        filtered_msg.angular.y = msg.angular.y * self.speed
+        filtered_msg.angular.z = msg.angular.z * self.speed
+        self.pub.publish(filtered_msg)
+    
+    def action_cb(self, goal):
+        self.speed *= goal.rate
+        result = VelocityFilterResult(speed = self.speed)
+        self.server.set_succeeded(result)
+        
+if __name__ == '__main__':
+    rospy.init_node('velocity_filter')
+    VelocityFilter()
+    rospy.spin()
