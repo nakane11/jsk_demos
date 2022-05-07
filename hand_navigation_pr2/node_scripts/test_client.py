99aa4341aee92c7b222da3de4c4871eed8e1921d
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-05-07 23:37:37 +0900

    Update test_client for give-hand-init

--- hand_navigation_pr2/node_scripts/test_client.py
+++ hand_navigation_pr2/node_scripts/test_client.py
@@ -1,13 +1,13 @@
 import rospy
 import actionlib
-from hand_navigation_pr2.msg import HandNavigationAction
-from hand_navigation_pr2.msg import HandNavigationGoal
+from hand_navigation_pr2.msg import GiveHandInitAction
+from hand_navigation_pr2.msg import GiveHandInitGoal
 
 rospy.init_node("give_hand_init_client")
 nav_client = actionlib.SimpleActionClient('give_hand_init',
-                                                HandNavigationAction)
+                                                GiveHandInitAction)
 nav_client.wait_for_server()
-nav_client.send_goal(HandNavigationGoal(task_id=10))
+nav_client.send_goal(GiveHandInitGoal(task_id=0))
 
 print(nav_client.get_result())
 
