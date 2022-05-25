c600b061be188b9ebbbef1ebded14e12d3841bfc
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-05-25 21:43:46 +0900

    Delete comment

--- hand_navigation_pr2/node_scripts/mux_selector_overwrite.py
+++ hand_navigation_pr2/node_scripts/mux_selector_overwrite.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
-
-import signal
-
 from std_msgs.msg import String
 from topic_tools.srv import MuxSelect, MuxAdd, MuxDelete
 import rospy
 
+
 class MuxOverwrite():
+
     def __init__(self):
-        # rospy.init_node('mux_selector_overwrite', disable_signals=False)
-        rospy.init_node('mux_selector_overwrite')
         self.before = '/teleop/cmd_vel'
         self.target_topic = rospy.get_param('~target', '/navigation/cmd_vel')
         rospy.wait_for_service('/vel_type_mux/select')
@@ -19,7 +16,6 @@ class MuxOverwrite():
         self.add_client('/navigation/cmd_vel_filtered')
         self.delete_client = rospy.ServiceProxy('/vel_type_mux/delete',MuxDelete)
         self.sub = rospy.Subscriber('/vel_type_mux/selected', String, self.cb)
-        rospy.on_shutdown(self.hook)
 
     def cb(self, msg):
         if msg.data == self.target_topic and self.before != self.target_topic:
@@ -28,22 +24,10 @@ class MuxOverwrite():
 
     def hook(self):
         self.delete_client('/navigation/cmd_vel_filtered')
+    
 
-    # def hook(self, signal=None, frame=None):
-    #     self.delete_client('/navigation/cmd_vel_filtered')
-
-    # def run(self):
-    #     rate = rospy.Rate(10)
-    #     # while True:
-    #     #     rate.sleep()
-    #     while not rospy.is_shutdown():
-    #         rate.sleep()
-                
 if __name__ == "__main__":
-    # rospy.init_node('mux_selector_overwrite', disable_signals=True)
-    # mo = MuxOverwrite()
-    # signal.signal(signal.SIGINT, mo.hook)
-    # mo.run()
-    # rospy.init_node('mux_selector_overwrite', disable_signals=False)
+    rospy.init_node('mux_selector_overwrite')
     mo = MuxOverwrite()
+    rospy.on_shutdown(mo.hook)
     rospy.spin()
