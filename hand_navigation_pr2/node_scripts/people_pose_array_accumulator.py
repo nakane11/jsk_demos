b57007980a4f5d4ddc0ef025953f934f6933e41c
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-06-14 12:22:28 +0900

    Fix lookup and change PoseStamped to PoseArray

--- hand_navigation_pr2/node_scripts/people_pose_array_accumulator.py
+++ hand_navigation_pr2/node_scripts/people_pose_array_accumulator.py
@@ -3,7 +3,7 @@
 from copy import deepcopy
 
 import numpy as np
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose, PoseArray
 from jsk_recognition_msgs.msg import PeoplePoseArray
 import rospy
 import PyKDL
@@ -21,20 +21,19 @@ class PeoplePoseArrayAccumulator():
 
         self.base_frame_id = rospy.get_param("~base_frame_id")
         rospy.loginfo("target frame_id: {}".format(self.base_frame_id))
-        self.pub = rospy.Publisher('~output', PoseStamped, queue_size=1)
+        self.pub = rospy.Publisher('~output', PoseArray, queue_size=1)
         self.sub = rospy.Subscriber('~input', PeoplePoseArray, self._cb)
+        self.pose_array = []
         self.last_published_time = rospy.Time.now()
 
     def _cb(self, msg):
-        header = deepcopy(msg.header)
-        header.frame_id = self.base_frame_id
-        pose_stamped_msg = PoseStamped(header=header)
         try:
             pykdl_transform_base_to_camera = tf2_geometry_msgs.transform_to_kdl(
                 self._tf_buffer.lookup_transform(
                     self.base_frame_id,
                     msg.header.frame_id,
-                    msg.header.stamp,
+                    # msg.header.stamp,
+                    rospy.Time.now(),
                     timeout=rospy.Duration(self._duration_timeout)))
         except (tf2_ros.LookupException,
                 tf2_ros.ConnectivityException,
@@ -42,43 +41,48 @@ class PeoplePoseArrayAccumulator():
             rospy.logwarn('{}'.format(e))
             return
 
+        center_array = []
         for person in msg.poses:
-            poses_len.append(len(person.poses))
-        if len(poses_len) <= 0:
-            return
-            
-        poses = []
-        person = msg.poses[index(max(poses_len))]
-        for pose in person.poses:
-            poses.append(
-                [pose.position.x,
-                 pose.position.y,
-                 pose.position.z])
-        if len(poses) < 5:
-            return
-        poses = np.array(poses)
-        center = np.sum(poses, axis=0) / len(poses)
-        if np.any(np.isnan(center)):
-            return
-        x, y, z = center[0], center[1], center[2]
-        x, y, z = pykdl_transform_base_to_camera * PyKDL.Vector(
-                x, y, z)
-        self.pose_array = np.append(self.pose_array, np.array([x, y, z]), axis = 0)
-
-        if rospy.Time.now() - self.last_published_time > rospy.Duration(3):
-            if len(self.pose_array) <= 0:
+            if len(person.poses) < 5:
                 return
-            pose_median = np.median(self.pose_array, axis = 0)
-            pose_stamped_msg.pose.position.x = pose_median[0]
-            pose_stamped_msg.pose.position.y = pose_median[1]
-            pose_stamped_msg.pose.position.z = pose_median[2]
-            pose_stamped_msg.pose.orientation.x = 0.0
-            pose_stamped_msg.pose.orientation.y = 0.0
-            pose_stamped_msg.pose.orientation.z = 0.0
-            pose_stamped_msg.pose.orientation.w = 1.0
-            self.pub.publish(pose_stamped_msg)
-            self.last_published_time = rospy.Time.now()
+            poses = []
+            for pose in person.poses:
+                poses.append(
+                    [pose.position.x,
+                     pose.position.y,
+                     pose.position.z])
+            poses = np.array(poses)
+            center = np.sum(poses, axis=0) / len(poses)
+            if np.any(np.isnan(center)):
+                return
+            x, y, z = center[0], center[1], center[2]
+            x, y, z = pykdl_transform_base_to_camera * PyKDL.Vector(
+                x, y, z)
+            center_array.append([x, y, z])
+        if center_array:
+            center_array = np.array(center_array)
+            x_array = np.abs(center_array[:, 0])
+            index = x_array.argmin()
+            self.pose_array.append(center_array[index, :].tolist())
 
+        if rospy.Time.now() - self.last_published_time > rospy.Duration(1):
+            header = deepcopy(msg.header)
+            header.frame_id = self.base_frame_id
+            pose_array_msg = PoseArray(header=header)
+            pose_msg = Pose()
+            if len(self.pose_array) > 0:
+                pose_median = np.median(np.array(self.pose_array), axis = 0)
+                pose_msg.position.x = pose_median[0]
+                pose_msg.position.y = pose_median[1]
+                pose_msg.position.z = pose_median[2]
+                pose_msg.orientation.x = 0.0
+                pose_msg.orientation.y = 0.0
+                pose_msg.orientation.z = 0.0
+                pose_msg.orientation.w = 1.0
+                pose_array_msg.poses.append(pose_msg)
+            self.pub.publish(pose_array_msg)
+            self.pose_array = []
+            self.last_published_time = rospy.Time.now()
 
 if __name__ == '__main__':
     rospy.init_node('people_pose_array_accumulator')
